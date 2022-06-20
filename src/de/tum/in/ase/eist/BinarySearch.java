8bbf877b957e3ac5f72aff18d1c7f4b8e257fbed
Emi <emi.mano@tum.de>
2022-06-20 15:38:42 +0200

    ab

--- src/de/tum/in/ase/eist/BinarySearch.java
+++ src/de/tum/in/ase/eist/BinarySearch.java
@@ -7,10 +7,10 @@ public class BinarySearch implements SearchStrategy {
 
 
     public int performSearch(List<Chapter> book, String name) {
-        for (int i = 0; i < book.size(); i++) {
+        for (Chapter chapter : book) {
 
-            if (Objects.equals(book.get(i).getName(), name)) {
-                return book.get(i).getPageNumber();
+            if (Objects.equals(chapter.getName(), name)) {
+                return chapter.getPageNumber();
             }
 
         }
