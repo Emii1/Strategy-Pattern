a3b70de1533c7a0b9c2a61a1dd397814e30ca87c
Emi <emi.mano@tum.de>
2022-06-20 14:28:05 +0200

    ab

--- src/de/tum/in/ase/eist/BinarySearch.java
+++ src/de/tum/in/ase/eist/BinarySearch.java
@@ -3,17 +3,17 @@ package de.tum.in.ase.eist;
 import java.util.List;
 import java.util.Objects;
 
-public class LinearSearch {
-   public List<Chapter> book;
-   public String name;
+public class BinarySearch {
+    public List<Chapter> book;
+    public String name;
 
 
     public  int performSearch(List<Chapter> book, String name ) {
         for (int i = 0; i < book.size(); i++) {
 
-              if(Objects.equals(book.get(i).getName(), name)){
-                  return  book.get(i).getPageNumber();
-              }
+            if(Objects.equals(book.get(i).getName(), name)){
+                return  book.get(i).getPageNumber();
+            }
 
         }
         return -1;
