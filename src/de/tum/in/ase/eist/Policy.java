3bd9ea145902d55f23507cd034df64cf5d76b371
Emi <emi.mano@tum.de>
2022-06-20 15:25:41 +0200

    ab

--- src/de/tum/in/ase/eist/Policy.java
+++ src/de/tum/in/ase/eist/Policy.java
@@ -7,11 +7,14 @@ public class Policy {
         this.context = context;
     }
 
-    public void configure(){
-        if (!context.isChaptersSortedByName()){
+    public void configure() {
+        if (!context.isChaptersSortedByName()) {
             LinearSearch linearSearch = new LinearSearch();
             context.setSearchAlgorithm(linearSearch);
+        } else {
+            BinarySearch binarySearch = new BinarySearch();
+            context.setSearchAlgorithm(binarySearch);
         }
-    }
 
+    }
 }
