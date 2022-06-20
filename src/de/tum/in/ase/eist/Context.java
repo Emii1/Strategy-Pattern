5e60f97cce6951ee442c6636f3864190d474b4a5
Emi <emi.mano@tum.de>
2022-06-20 14:50:40 +0200

    ab

---
+++ src/de/tum/in/ase/eist/Context.java
@@ -0,0 +1,34 @@
+package de.tum.in.ase.eist;
+
+import java.util.List;
+
+public class Context {
+    private List<Chapter> book;
+
+
+    public Context(List<Chapter> book) {
+        this.book = book;
+    }
+
+    public List<Chapter> getBook() {
+        return book;
+    }
+
+    public void setBook(List<Chapter> book) {
+        this.book = book;
+    }
+
+    public boolean isChaptersSortedByName() {
+
+        for (int i = 0; i < book.size(); i++) {
+            for (int j = i + 1; j < book.size(); j++) {
+                if (book.get(i).getName().compareTo(book.get(j).getName()) > 0) {
+                    return false;
+                }
+
+            }
+
+        }
+        return true;
+    }
+}
